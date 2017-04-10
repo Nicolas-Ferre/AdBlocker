490885a1acd48a834b47530918f710c4b8edd358
Shizenokami <theo.lognoz@telecomnancy.net>
2017-04-10 14:56:22 +0200

    selection des hostnames

--- src/extraction.c
+++ src/extraction.c
@@ -11,31 +11,29 @@ void decoupageListe(char *motsList[x /* a remplacer par la taille de la liste? *
 	FILE *fp;
    	fp=fopen("../assets/easylist.txt","r"); 
 	if(fp!=NULL) {
-	   while ( fgets ( ligne, 70, fp ) != NULL ) { 
- 				 	
-   			for(i = 0; i < (int)strlen(ligne); i++) {
-        		if(ligne[i]=='\n') {
-       				motsList[prochainMot][j] = '\0';
-       				prochainMot++;
-        			j = 0;
-        		}
-				
-        		else if(ligne[i] != '\0' ) {
-					motsDico[prochainMot][j] = ligne[i];
-					j++;
-       			}		
+	   while ( fgets ( ligne, 70, fp ) != NULL ) {
+			char c = ligne[0];
+			if ( c == '.' || c == '-' || c == '_'  || (c>='1' && c<='9') || (c>='A' && c<='Z') || (c>='a' && c<='z') ) {
+ 				printf("%s",ligne);
+				for(i = 0; i < (int)strlen(ligne); i++) {
+					if(ligne[i]=='\n') {
+						motsList[prochainMot][j] = '\0';
+						prochainMot++;
+						j = 0;
+					}
+					
+					else if(ligne[i] != '\0' ) {
+						motsDico[prochainMot][j] = ligne[i];
+						j++;
+					}		
+				}
 			}
-			
-   		motsDico[prochainMot][j] = '\0';
       	}
 	}	
 	 	
-    	 else {
-      	
-          	fputs("erreur: mauvais chemin d'acces pour la liste\n",stderr);
-        	
-          		
-        }
+    else {
+        fputs("erreur: mauvais chemin d'acces pour la liste\n",stderr);
+    }
 	fclose(fp);
 	fp=NULL;
 	
